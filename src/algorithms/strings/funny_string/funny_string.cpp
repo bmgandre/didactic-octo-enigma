8736fc67f81151a4d02e99e58a2af70fa331b5b6
André B. M. Gomes <bmg.andre@gmail.com>
2020-11-27 22:25:12 -0300

    Add algorithms/strings/funny_string
    Signed-off-by: André B. M. Gomes <bmg.andre@gmail.com>

---
+++ src/algorithms/strings/funny_string/funny_string.cpp
@@ -0,0 +1,24 @@
+#include "funny_string.h"
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using hackerrank::bmgandre::algorithms::strings::funny_string;
+
+std::string funny_string::solve(std::string input)
+{
+	std::string reverse;
+	reverse.resize(input.size());
+
+	std::reverse_copy(std::begin(input), std::end(input), std::begin(reverse));
+
+	for (auto i = 0ul; i < input.length() - 1; i++)
+	{
+		if (std::abs(input[i] - input[i + 1]) != std::abs(reverse[i] - reverse[i + 1]))
+		{
+			return "Not Funny";
+		}
+	}
+
+	return "Funny";
+}
---
+++ src/algorithms/strings/funny_string/funny_string.h
@@ -0,0 +1,22 @@
+#ifndef FUNNY_STRING_H
+#define FUNNY_STRING_H
+
+#include <string>
+
+namespace hackerrank {
+namespace bmgandre {
+namespace algorithms {
+namespace strings {
+
+class funny_string
+{
+public:
+	static std::string solve(std::string input);
+};
+
+} // namespace algorithms
+} // namespace strings
+} // namespace bmgandre
+} // namespace hackerrank
+
+#endif // FUNNY_STRING_H
